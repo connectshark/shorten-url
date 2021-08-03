9959831e2f808aa89ccdd8a1a67ce1b8b53f41b3
connectshark <bobhus394@gmail.com>
2021-08-03 13:30:11 +0800

    新增基礎功能

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,36 @@
 <template>
-  <img alt="Vue logo" src="../assets/logo.png">
-  <h2>Hi! 快速使用vue3做出網頁template</h2>
+<input v-model="url" type="url"/>
+<input type="button" value="送出" @click="submit">
+<p v-if="!loading">{{result}}</p>
 </template>
+
+<script>
+import { ref } from 'vue'
+import axios from 'axios'
+
+export default {
+  setup () {
+    const url = ref('')
+    const result = ref('')
+    const loading = ref(false)
+    const submit = () => {
+      if (url.value === '') return
+      loading.value = true
+      axios.get(process.env.VUE_APP_URL + '/shortenUrl?url=' + url.value)
+        .then(res => {
+          result.value = res.data.short_url
+          loading.value = false
+        })
+    }
+    return {
+      url,
+      submit,
+      result,
+      loading
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
