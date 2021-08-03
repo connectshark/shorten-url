0231220dcfa2d8e548467c02b8e4c8bda9c63310
connectshark <bobhus394@gmail.com>
2021-08-03 17:58:53 +0800

    新增css內容

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,37 @@
 <template>
+<div class="container">
+  <div class="row">
+    <div class="col-12 text-center fs-2">
+      <h1>短網址服務</h1>
+    </div>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</div>
+
   <router-view></router-view>
 </template>
 
@@ -11,12 +44,12 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.2/iconfont/material-icons.min.css');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgba(0, 0, 0, 0.84);
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,41 @@
 <template>
-<input v-model="url" type="url"/>
-<input type="button" value="送出" @click="submit">
-<p v-if="!loading">{{result}}</p>
+<div class="container">
+  <form class="row" @submit="submit">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">填入欲縮短的網址</label>
+      <input
+        type="url"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="urlHelp"
+        required
+        placeholder="https://www.google.com/"
+        pattern="https://.*"
+        v-model.trim="url"
+      >
+    </div>
+    <div class="btn-group col-4 mx-auto" role="group" aria-label="Basic outlined example">
+      <button type="submit" class="btn btn-outline-primary">送出</button>
+      <button type="reset" class="btn btn-outline-secondary">清除</button>
+    </div>
+  </form>
+  <div class="row py-5" v-if="result">
+    <label for="short-url" class="form-label">縮短網址</label>
+    <div class="col-12">
+      <input class="form-control"
+        type="text"
+        v-model="result"
+        id="short-url"
+        aria-label="唯獨內容"
+        readonly
+      >
+    </div>
+    <div class="col-2 mx-auto py-3">
+      <button type="submit" class="btn btn-outline-primary" @click="copy">複製</button>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -22,11 +56,23 @@ export default {
           loading.value = false
         })
     }
+
+    const copy = () => {
+      const clipboardy = require('clipboardy')
+      clipboardy.write(result.value)
+        .then(() => {
+          alert('複製成功')
+        })
+        .catch(() => {
+          alert('複製失敗 請通報維護工程師')
+        })
+    }
     return {
       url,
       submit,
       result,
-      loading
+      loading,
+      copy
     }
   }
 }
